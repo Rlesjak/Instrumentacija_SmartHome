70f1c86cc9cbf8307407bf650d3ab14e9d8bd35e
Rlesjak <lesjak.robert1@gmail.com>
2022-04-08 18:16:29 +0200

    logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,68 +7,149 @@
 #include <sh_hdc2080.h>
 #include <communication.h>
 
-#define MES_INTERVAL 200
-#define AVG_INTERVAL 6000
+#define TEMP_MES_INTERVAL 60000
+#define TEMP_AVG_INTERVAL 600000
+#define TEMP_BUFF_SIZE ROLLING_BUFFER_SIZE(TEMP_AVG_INTERVAL, TEMP_MES_INTERVAL)
+
+#define LIG_PR_MES_INTERVAL 1000
+#define LIG_PR_AVG_INTERVAL 10000
+#define LIG_PR_BUFF_SIZE ROLLING_BUFFER_SIZE(LIG_PR_AVG_INTERVAL, LIG_PR_MES_INTERVAL)
+
+
 #define PRINT_INTERVAL 1000
 
-#define OPT_BUFF_SIZE ROLLING_BUFFER_SIZE(AVG_INTERVAL, MES_INTERVAL)
+#define MIN_LUX 750
+#define PRESS_TRESHOLD 6
+#define LOW_TEMP 22
+#define HIGH_TEMP 24
 
-float _lightBuff[OPT_BUFF_SIZE] = {0.0f};
-fifo_controller_t lightBuff = {OPT_BUFF_SIZE, _lightBuff, 0};
+float _tempBuff[TEMP_BUFF_SIZE] = {0.0f};
+fifo_controller_t tempBuff = {TEMP_BUFF_SIZE, _tempBuff, 0};
 
-float _tempBuff[OPT_BUFF_SIZE] = {0.0f};
-fifo_controller_t tempBuff = {OPT_BUFF_SIZE, _tempBuff, 0};
-
-float _pressureBuff[OPT_BUFF_SIZE] = {0.0f};
-fifo_controller_t pressureBuff = {OPT_BUFF_SIZE, _pressureBuff, 0};
-
-float _temp2Buff[OPT_BUFF_SIZE] = {0.0f};
-fifo_controller_t temp2Buff = {OPT_BUFF_SIZE, _temp2Buff, 0};
-
-float _humiBuff[OPT_BUFF_SIZE] = {0.0f};
-fifo_controller_t humiBuff = {OPT_BUFF_SIZE, _humiBuff, 0};
+float _humiBuff[TEMP_BUFF_SIZE] = {0.0f};
+fifo_controller_t humiBuff = {TEMP_BUFF_SIZE, _humiBuff, 0};
 
 
-unsigned long lastMesTimestamp;
+
+float _lightBuff[LIG_PR_BUFF_SIZE] = {0.0f};
+fifo_controller_t lightBuff = {LIG_PR_BUFF_SIZE, _lightBuff, 0};
+
+float _pressureBuff[LIG_PR_BUFF_SIZE] = {0.0f};
+fifo_controller_t pressureBuff = {LIG_PR_BUFF_SIZE, _pressureBuff, 0};
+
+// Timestamps
+unsigned long lastTempTimestamp;
+unsigned long lastPressTimestamp;
+
 unsigned long lastPrintTimestamp;
 unsigned long currentTimestamp;
+
+// State
+bool isLightOn=false;
+float lastPressure = 0.0f;
+float averages[4];
+
+
 
 void setup() {
 	comm::configure(9600);
 	opt::configure();
 	dps::configure();
 	hdc::configure();
-	lastMesTimestamp = millis();
+	lastTempTimestamp = millis();
+	lastPressTimestamp = millis();
 	lastPrintTimestamp = millis();
+
+	delay(1000);
+	float temp, humi;
+	hdc::read( &temp, &humi );
+
+	for (int i = 0; i < TEMP_BUFF_SIZE; i++)
+	{
+		_tempBuff[i] = temp;
+		_humiBuff[i] = humi;
+	}
 }
 
 void loop() {
 	currentTimestamp = millis();
+
+	// Send data over serial
 	if (currentTimestamp - lastPrintTimestamp > PRINT_INTERVAL)
 	{
-		float averages[] = {
-			fifo_getAverage( &lightBuff ), 
-			fifo_getAverage( &tempBuff ), 
-			fifo_getAverage( &pressureBuff ),
-			fifo_getAverage( &temp2Buff ),
-			fifo_getAverage( &humiBuff )
-			};
+		averages[0] = fifo_getAverage( &lightBuff );
+		averages[1] = fifo_getAverage( &tempBuff );
+		averages[2] = fifo_getAverage( &pressureBuff );
+		averages[3] = fifo_getAverage( &humiBuff );
 		
-		comm::sendPacket(averages, 5);
+		comm::sendPacket(averages, 4);
 
 		lastPrintTimestamp = currentTimestamp;
 	}
 	
-	if (currentTimestamp - lastMesTimestamp > MES_INTERVAL)
+	// Measure temperature and humidity
+	if (currentTimestamp - lastTempTimestamp > TEMP_MES_INTERVAL)
 	{
-		float temp, temp2, pressure, humi;
-		dps::read( &temp, &pressure );
-		hdc::read( &temp2, &humi );
-		fifo_push( &lightBuff, opt::read() );
+		float temp, humi;
+		hdc::read( &temp, &humi );
+
 		fifo_push( &tempBuff, temp );
-		fifo_push( &pressureBuff, pressure );
-		fifo_push( &temp2Buff, temp2 );
 		fifo_push( &humiBuff, humi );
-		lastMesTimestamp = currentTimestamp;
+
+		// Detect temperature change
+		if (temp >= HIGH_TEMP){	
+			// Turn heating off
+			comm::sendEvent(30);
+		}
+		else if (temp < LOW_TEMP)
+		{
+			// Turn heating on
+			comm::sendEvent(31);
+		}
+
+		lastTempTimestamp = currentTimestamp;
+	}
+
+	// Mesure light and pressure
+	if (currentTimestamp - lastPressTimestamp > LIG_PR_MES_INTERVAL)
+	{
+		float pressure, light, _temp;
+		dps::read( &_temp, &pressure );
+		light = opt::read();
+
+		fifo_push( &lightBuff, light);
+		fifo_push( &pressureBuff, pressure );
+
+		// Detect pressure change
+		if (lastPressure - pressure > PRESS_TRESHOLD) {
+			// Pressure drop
+			comm::sendEvent(20);
+		}
+		else if (pressure - lastPressure > PRESS_TRESHOLD){
+			// Pressure rise
+			comm::sendEvent(21);
+		}
+		lastPressure = pressure;
+
+
+		// Detect light change
+		if(isLightOn == true){
+			if(light >= MIN_LUX){
+				// Turn light on
+				comm::sendEvent(10);
+				isLightOn=false;
+			}
+			
+		}
+		else{
+			if(light < MIN_LUX){
+				// Turn light off
+				comm::sendEvent(11);
+				isLightOn=true;
+			}
+
+		}
+
+		lastPressTimestamp = currentTimestamp;
 	}
 }
